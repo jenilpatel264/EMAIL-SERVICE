e3406fae484ab79546d2c6a0f0044c7a29561933
JENIL  PATEL <jenilpatel264>
2023-09-02 16:42:14 +0530

    docker last

--- src/main/java/com/email/SpringEmailServiceApplication.java
+++ src/main/java/com/email/SpringEmailServiceApplication.java
@@ -2,12 +2,19 @@ package com.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class SpringEmailServiceApplication {
+public class SpringEmailServiceApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringEmailServiceApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmailServiceApplication.class, args);
 	}
